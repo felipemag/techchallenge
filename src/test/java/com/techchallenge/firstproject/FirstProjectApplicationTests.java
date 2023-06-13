a8a89988c3e705edf9250b0ca4b5eabd7342a1b9
Felipe <eu@felipemagalha.es>
2023-06-13 20:25:39 -0300

    Initial commit

---
+++ src/test/java/com/techchallenge/firstproject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
