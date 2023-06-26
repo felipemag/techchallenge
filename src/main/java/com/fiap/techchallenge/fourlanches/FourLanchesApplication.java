430ed7a5ff5cc914778bf19ad7739c43157e828e
Lucas Zingano <lucasbz@gmail.com>
2023-06-25 22:09:16 -0300

    feat: Acerta test inicial

--- src/main/java/com/fiap/techchallenge/fourlanches/FourLanchesApplication.java
+++ src/main/java/com/fiap/techchallenge/fourlanches/FourLanchesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FourlanchesApplication {
+public class FourLanchesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FourlanchesApplication.class, args);
+		SpringApplication.run(FourLanchesApplication.class, args);
 	}
 
 }
