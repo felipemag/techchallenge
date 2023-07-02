2ed85f5ed7986bf0585edee8216a49a893103cd6
Lucas Zingano <lucasbz@gmail.com>
2023-07-02 19:42:54 -0300

    feat: Adiciona Order entity

---
+++ src/main/java/com/fiap/techchallenge/fourlanches/domain/entities/Order.java
@@ -0,0 +1,18 @@
+package com.fiap.techchallenge.fourlanches.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Order {
+
+    private Long id;
+    private BigDecimal price;
+
+
+}
