9850684cc9f3878b4390bba929809922476e18a0
Lucas Zingano <lucasbz@gmail.com>
2023-07-02 23:09:12 -0300

    refactor: Adiciona nome da tabela customers a CustomerJpaEntity

--- src/main/java/com/fiap/techchallenge/fourlanches/adapter/driven/data/entities/CustomerJpaEntity.java
+++ src/main/java/com/fiap/techchallenge/fourlanches/adapter/driven/data/entities/CustomerJpaEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Table(name = "customers")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
