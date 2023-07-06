2a5f6517ac32bf300580e2e85bc978607e0a33e0
Augusto Silva <augusto.a.silva@hotmail.com>
2023-07-06 19:59:06 -0300

    feat: Adiciona migration para criar tabela de customer

---
+++ src/main/resources/db/migration/V5__Create_Customer_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers (
+    id  SERIAL PRIMARY KEY NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    document varchar(255) NOT NULL
+);
